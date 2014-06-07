2de1a346dd13f7bc6206c61ff5ce94805f3c28bd
Vi <turcanu.victor8@gmail.com>
2014-06-08 00:37:45 +0300

    IDEL2: Lab 2 initial project

---
+++ IDE/Lab#2/Lab2/Lab2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate   = 0;     // -10...10
+            //synthesizer.SpeakAsync("Welcome to Simulatron!");
+        }
+    }
+}
