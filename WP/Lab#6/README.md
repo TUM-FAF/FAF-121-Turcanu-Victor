87f3b054d066d14f55343f849b5c61757931ee6a
Vi <turcanu.victor8@gmail.com>
2014-06-19 07:32:52 +0300

    L6WP: Added source and README

---
+++ WP/Lab#6/README.md
@@ -0,0 +1,23 @@
+Laboratory Work Nr. 6
+====================
+
+Developing using C#. WinRT
+------------
+
+Completed Mandatory Objectives:
+------------------------------
+
+*	Create an Win32 or WinRT application using C#
+
+Objectives With Points
+------------------------
+
+*	Create a report (3pt)
+*	Use WinRT (3pt)
+*	Created a texty to soudn display (1pt)
+
+
+The theory of workflow:
+-----------------------------
+
+I've chose to do the application from the second laboratory work on IDE, because there I've used WPF in order to perform the tasks. WPF is quite easy to use and a lot of interactions can be done without C#. But this wasn't my purpose, I've got accostumed to the .net Framewok and it was a nice experience for me.
---
+++ WP/Lab#6/Source/Lab2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate   = 0;     // -10...10
+            synthesizer.SpeakAsync("The Simulatron has started!");
+        }
+
+        private void CheckBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            synthesizer.SpeakAsync("Put your seat belts.");
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Expander1.IsExpanded = false;
+            CheckBox1.IsChecked = false;
+            synthesizer.Volume = 100;
+            synthesizer.Rate = 0;
+            synthesizer.SpeakAsync("The system has been successfully reset.");
+        }
+
+
+    }
+}
